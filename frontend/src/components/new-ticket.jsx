b2c045ea8ac50851679c3bb08267619d783c0485
emrah-i <ibraem1026@gmail.com>
2023-11-04 18:34:12 -0400

    New ticket contributors

--- frontend/src/components/new-ticket.jsx
+++ frontend/src/components/new-ticket.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 function NewTicket() {
 
     const [ inputOption, setInputOption ] = useState('text')
+    const [ contributors, setContributors ] = useState([])
+    const [ descriptionChars, setDescriptionChars ] = useState(0)
+    const selectContributors = useRef(null)
 
     const fakeProjectNames = [
         "GreenThumb",
@@ -27,7 +30,26 @@ function NewTicket() {
         "Taylor Brown",
         "Jordan Casey"
       ]
-      
+
+    function addContributor() {
+        const ind = selectContributors.current.value;
+    
+        if (ind === 'none' || contributors.includes(ind)) {
+            return;
+        }
+    
+        setContributors(prevState => {
+            return [...prevState, ind];
+        });
+    }
+
+    function removeContributor(ind) {
+
+        setContributors(prevState => {
+            const newState = prevState.filter(item => item !== ind)
+            return [...newState];
+        });
+    }
 
     return (<div className="bg-main-color mx-5 mt-5 pt-3 pb-5 px-5 shadow-md rounded-lg">
                 <p className="heading border-gray-200 border-solid border-b-2 pb-3">New Ticket</p>
@@ -42,22 +64,23 @@ function NewTicket() {
                         </label>
                     </div>
                     <div className="flex justify-around">
-                        <input className="w-2/5 shadow-md py-1.5 px-3 text-black bg-gray-100 rounded-full disabled:opacity-50" disabled={!(inputOption === 'text')} type="text" placeholder="Project"></input>
-                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" disabled={(inputOption === 'text')} type="text" placeholder="Project">
-                            <option selected disabled >Select a project</option>
-                            {fakeProjectNames.map((element)=>{
-                                return <option>{element}</option>
+                        <input className="w-2/5 shadow-md py-1.5 px-3 text-black bg-gray-100 rounded-full disabled:opacity-50 placeholder:text-gray-500" disabled={!(inputOption === 'text')} type="text" placeholder="Project"></input>
+                        <select defaultValue={'none'} className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" disabled={(inputOption === 'text')} type="text" placeholder="Project">
+                            <option value={'none'} disabled >Select a project</option>
+                            {fakeProjectNames.map((element, index)=>{
+                                return <option key={index}>{element}</option>
                             })}
                         </select>
                     </div>
                     <div>
                         <label>Description:</label>
-                        <textarea className="w-full shadow-md py-1.5 px-3 text-black bg-gray-100 rounded-lg" maxLength={1600} type="text"></textarea>
+                        <textarea className="w-full shadow-md py-1.5 px-3 text-black bg-gray-100 rounded-lg" maxLength={1600} type="text" onChange={(event)=>{setDescriptionChars(event.target.value.length)}}></textarea>
+                        <p className="text-xs">Length: <span>{descriptionChars}</span> / 1600</p>
                     </div>
                     <div>
                         <label className="mr-3">Type:</label>
-                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
-                            <option selected disabled >Select a project</option>
+                        <select defaultValue={'none'} className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option value={'none'} disabled >Select a project</option>
                             <option>Bug</option>
                             <option>Feature Request</option>
                             <option>Issue</option>
@@ -66,8 +89,8 @@ function NewTicket() {
                     </div>
                     <div>
                         <label className="mr-3">Priority:</label>
-                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
-                            <option selected disabled >Select a priority</option>
+                        <select defaultValue={'none'} className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option value={'none'} disabled >Select a priority</option>
                             <option>Low</option>
                             <option>Medium</option>
                             <option>High</option>
@@ -76,8 +99,8 @@ function NewTicket() {
                     </div>
                     <div>
                         <label className="mr-3">Status:</label>
-                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
-                            <option selected disabled >Select a status</option>
+                        <select defaultValue={'none'} className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option value={'none'} disabled >Select a status</option>
                             <option>New Ticket</option>
                             <option>In Development</option>
                             <option>Code Review</option>
@@ -88,13 +111,18 @@ function NewTicket() {
                     </div>
                     <div>
                         <label className="mr-3">Contributers :</label>
-                        <select className="w-2/5 mr-3 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
-                            <option selected disabled >Select a team member</option>
-                            {fakeTeam.map((element)=>{
-                                return <option>{element}</option>
+                        <select defaultValue={'none'} className="w-2/5 mr-3 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project" ref={selectContributors} >
+                            <option value={'none'} disabled >Select a team member</option>
+                            {fakeTeam.map((element, index)=>{
+                                return <option key={index}>{element}</option>
                             })}
                         </select>
-                        <button className="shadow-md bg-sec-color text-main-color rounded-lg py-1 px-2 hover:rounded-xl duration-300" type="button">Add</button>
+                        <button className="shadow-md bg-sec-color text-main-color rounded-lg py-1 px-2 hover:rounded-xl duration-300" type="button" onClick={addContributor}>Add</button>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        {contributors.map((element, index)=>{
+                            return <div key={index} className="flex justify-between items-center w-fit bg-gray-100 py-1 px-2 rounded-lg shadow"><p className="pr-3">{element}</p><button type="button" onClick={()=>removeContributor(element)} ><i className="pl-3 text-red-500 text-lg fa-solid fa-xmark"></i></button></div>
+                        })}
                     </div>
                 </form>
             </div>)
