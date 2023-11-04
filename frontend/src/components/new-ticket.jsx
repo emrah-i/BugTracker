7b971a4d9e8f6682c160f5566e1c2aa8b8f7f8c5
emrah-i <ibraem1026@gmail.com>
2023-11-04 16:13:44 -0400

    Updating ticket

--- frontend/src/components/new-ticket.jsx
+++ frontend/src/components/new-ticket.jsx
@@ -1,18 +1,101 @@
+import { useState } from "react"
 
 function NewTicket() {
-    return (<div className="bg-main-color mx-5 mt-5 py-3 px-5 shadow-md rounded-lg">
-                <form className="flex flex-col">
-                    <p className="heading">New Ticket</p>
+
+    const [ inputOption, setInputOption ] = useState('text')
+
+    const fakeProjectNames = [
+        "GreenThumb",
+        "ArtisanBreadClub",
+        "CodeCollab",
+        "FitTrack",
+        "MindSpa",
+        "BudgetMaster",
+        "PetRescue",
+        "QuickTutor",
+        "EcoTravel",
+        "HomeChef"
+    ];    
+    
+    const fakeTeam = [
+        "Alex Johnson",
+        "Chris Smith",
+        "Jamie Lee",
+        "Morgan Blake",
+        "Pat Taylor",
+        "Sam Rivera",
+        "Taylor Brown",
+        "Jordan Casey"
+      ]
+      
+
+    return (<div className="bg-main-color mx-5 mt-5 pt-3 pb-5 px-5 shadow-md rounded-lg">
+                <p className="heading border-gray-200 border-solid border-b-2 pb-3">New Ticket</p>
+                <form className="flex flex-col gap-3 w-2/3 mx-auto">
                     {/* Allow choice between current projects and new projects */}
-                    <label>
-                    <input type="radio" name="inputType" value="text" checked onclick="toggleInputType('text')" /> Text Input
-                    </label>
-                    <label>
-                    <input type="radio" name="inputType" value="dropdown" onclick="toggleInputType('dropdown')" /> Dropdown
-                    </label>
-                    <input type="text" placeholder="Project"></input>
-                    <input type="text"></input>
-                    <input type="text"></input>
+                    <div className="flex justify-around">
+                        <label>
+                        <input type="radio" name="inputType" value="text" defaultChecked onClick={()=>setInputOption('text')} /> New Project
+                        </label>
+                        <label className="text-left">
+                        <input type="radio" name="inputType" value="dropdown" onClick={()=>setInputOption('dropdown')} /> Existing Projects
+                        </label>
+                    </div>
+                    <div className="flex justify-around">
+                        <input className="w-2/5 shadow-md py-1.5 px-3 text-black bg-gray-100 rounded-full disabled:opacity-50" disabled={!(inputOption === 'text')} type="text" placeholder="Project"></input>
+                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" disabled={(inputOption === 'text')} type="text" placeholder="Project">
+                            <option selected disabled >Select a project</option>
+                            {fakeProjectNames.map((element)=>{
+                                return <option>{element}</option>
+                            })}
+                        </select>
+                    </div>
+                    <div>
+                        <label>Description:</label>
+                        <textarea className="w-full shadow-md py-1.5 px-3 text-black bg-gray-100 rounded-lg" maxLength={1600} type="text"></textarea>
+                    </div>
+                    <div>
+                        <label className="mr-3">Type:</label>
+                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option selected disabled >Select a project</option>
+                            <option>Bug</option>
+                            <option>Feature Request</option>
+                            <option>Issue</option>
+                            <option>New Project</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className="mr-3">Priority:</label>
+                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option selected disabled >Select a priority</option>
+                            <option>Low</option>
+                            <option>Medium</option>
+                            <option>High</option>
+                            <option>Critical</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className="mr-3">Status:</label>
+                        <select className="w-2/5 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option selected disabled >Select a status</option>
+                            <option>New Ticket</option>
+                            <option>In Development</option>
+                            <option>Code Review</option>
+                            <option>Testing</option>
+                            <option>Deployed</option>
+                            <option>Closed</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className="mr-3">Contributers :</label>
+                        <select className="w-2/5 mr-3 shadow-md py-1.5 pl-3 border-r-8 border-gray-100 text-black bg-gray-100 rounded-full disabled:opacity-50" type="text" placeholder="Project">
+                            <option selected disabled >Select a team member</option>
+                            {fakeTeam.map((element)=>{
+                                return <option>{element}</option>
+                            })}
+                        </select>
+                        <button className="shadow-md bg-sec-color text-main-color rounded-lg py-1 px-2 hover:rounded-xl duration-300" type="button">Add</button>
+                    </div>
                 </form>
             </div>)
 }
--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -7,12 +7,12 @@ function SignUp() {
                 <div className="flex flex-col justify-between items-center rounded-lg shadow-2xl w-1/3 h-4/6 text-main-color bg-sec-color p-5">
                     <form className="w-3/5 h-full flex flex-col justify-center gap-3 items-center">
                         <p className="text-3xl font-semibold">Sign Up:</p>
-                        <input className="w-full rounded-lg p-1.5 text-black" type="text" name="username" placeholder="Username" />
-                        <input className="w-full rounded-lg p-1.5 text-black" type="email" name="email" placeholder="Email" />
-                        <input className="w-full rounded-lg p-1.5 text-black" type="email" name="email" placeholder="Confirm Email" />
-                        <input className="w-full rounded-lg p-1.5 text-black" type="password" name="password" placeholder="Password" />
-                        <input className="w-full rounded-lg p-1.5 text-black mb-2" type="password" name="confirm" placeholder="Confirm Password" />
-                        <button className="text-lg text-sec-color bg-main-color rounded-lg py-1 px-2 hover:rounded-xl duration-300" type="button">Sign Up</button>
+                        <input className="signup-input" type="text" name="username" placeholder="Username" />
+                        <input className="signup-input" type="email" name="email" placeholder="Email" />
+                        <input className="signup-input" type="email" name="email" placeholder="Confirm Email" />
+                        <input className="signup-input" type="password" name="password" placeholder="Password" />
+                        <input className="signup-input mb-2" type="password" name="confirm" placeholder="Confirm Password" />
+                        <button className="text-lg text-sec-color shadow-md bg-main-color rounded-lg py-1 px-2 hover:rounded-xl duration-300" type="button">Sign Up</button>
                     </form>
                     <div className="flex flex-col items-center">
                         <a>Already have an account? <a className="underline" href="/login">Login</a></a>
